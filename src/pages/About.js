bd12906d52bb897b9998d86224732ca8d075fd30
Jose Del Rosario <delrosa1@go.stockton.edu>
2021-12-18 06:11:59 -0500

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est quod
+        labore non, natus quasi itaque provident harum doloremque alias magni
+        debitis aut saepe in sequi laborum quaerat! Mollitia error eos repellat
+        sit aperiam, dolor quas molestiae at! Laborum, fugit error!
+      </p>
+    </section>
+  );
+};
+
+export default About;
